68343a5b7a8e106768abb307916a82ff3d4ac476
Lev# <mr.lmartirosyan888@gmail.com>
2019-01-31 00:38:16 +0400

    first relise

---
+++ GoTo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+        label:
+            counter++;
+            Console.WriteLine("counter = {0}", counter);
+            if (counter < 3)
+            {
+                goto label;
+            }
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+    }
+}
